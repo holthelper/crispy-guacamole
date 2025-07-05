062860e252adca9ab453e56055ba79c55d30a436
Austin Adams <holthelper@users.noreply.github.com>
2025-07-05 04:27:23 -0700

    Menu Change
    + Changed Birria Tacos > Birria Taco (Tray)
    + Changed Elote > Elote (Cup)
    + Added Elote (Cob)
    + Added Hot Cheeto Boat

--- js/items.js
+++ js/items.js
@@ -6,27 +6,39 @@ export const items = [
     Enabled: true
   },
   {
-    Name: "Birra Tacos",
+    Name: "Birria Taco (Tray)",
     Image: "./img/tray birria.jpg",
     Price: 17.00,
-    Enabled: true
-  },
-  {
-    Name: "Elotes",
-    Image: "./img/elote.jpg",
-    Price: 5.00,
-    Enabled: true
-  },
-  {
-    Name: "Agua Fresca",
-    Image: "./img/agua-fresca.jpg",
-    Price: 6.00,
     Enabled: true
   },
   {
     Name: "Birria Consomé",
     Image: "./img/consome.jpg",
     Price: 1.00,
+    Enabled: true
+  },
+  {
+    Name: "Elotes (Cup)",
+    Image: "./img/elote cup.jpg",
+    Price: 5.00,
+    Enabled: true
+  },
+  {
+    Name: "Elotes (Cob)",
+    Image: "./img/elote cob.jpg",
+    Price: 6.00,
+    Enabled: true
+  },
+  {
+    Name: "Hot Cheeto Boat",
+    Image: "./img/cheeto boat.jpg",
+    Price: 10.00,
+    Enabled: true
+  },
+  {
+    Name: "Agua Fresca",
+    Image: "./img/agua-fresca.jpg",
+    Price: 6.00,
     Enabled: true
   }
 ];
